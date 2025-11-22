efeb01b998bb09ee0ce6222721edcdf18c1929c4
Luiz Eugênio Filho <43796789+lesaf92@users.noreply.github.com>
2025-11-22 02:54:48 -0300

    install script update

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+# Basic sudo apt install stuff
 sudo apt update
-sudo apt install net-tools nmap iftop screen
+sudo apt install net-tools nmap iftop screen tree ncdu htop locate
+
+# Can I use curl inside curl?
+curl -fsSL https://tailscale.com/install.sh | sh
