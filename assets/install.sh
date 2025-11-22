4a9047fe7c891338622af115b9d89c895f5e3ea6
lesaf92 <43796789+lesaf92@users.noreply.github.com>
2025-11-22 06:36:59 +0000

    Deploying to gh-pages from @ lesaf92/lesaf92.github.io@bae2d81b6b7008d431b6084c67b4cd5acb6e8895 🚀

--- assets/install.sh
+++ assets/install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e
+
 # Check if the script is running as root
 if [ "$EUID" -ne 0 ]; then
-  echo "This script requires root privileges. Please run with sudo."
-  exec sudo bash "$0" "$@"
-  exit 1 # Should not be reached if sudo is successful
+  echo "Error: Please run this script as root."
+  echo "Usage: sudo ./install.sh"
+  exit 1
 fi
+
 echo "Running with root privileges."
 # Basic sudo apt install stuff
 apt update
-apt install net-tools nmap iftop screen tree ncdu htop locate openssh-server
+apt install -y net-tools nmap iftop screen tree ncdu htop locate openssh-server
 
 # Can I use curl inside curl?
 curl -fsSL https://tailscale.com/install.sh | sh
