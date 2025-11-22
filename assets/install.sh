75e95f98c8ce2884ccbc9c5cd1bd39d080bd1a92
Luiz Eugênio Filho <43796789+lesaf92@users.noreply.github.com>
2025-11-22 10:29:51 -0300

    install script update 5

--- assets/install.sh
+++ assets/install.sh
@@ -5,14 +5,13 @@ set -e
 # Check if the script is running as root
 if [ "$EUID" -ne 0 ]; then
   echo "Error: Please run this script as root."
-  echo "Usage: sudo ./install.sh"
   exit 1
 fi
 
 echo "Running with root privileges."
 # Basic sudo apt install stuff
-apt update
-apt install -y net-tools nmap iftop screen tree ncdu htop locate openssh-server
+sudo apt update
+sudo apt install -y net-tools nmap iftop screen tree ncdu htop locate openssh-server
 
 # Can I use curl inside curl?
 curl -fsSL https://tailscale.com/install.sh | sh
